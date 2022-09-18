50f824a883273a963461751c1ab96e4ccd134bba
George Yin Xu <georgeyinxu@gmail.com>
2022-09-18 21:49:32 +0800

    Initial Commit

---
+++ slots-game/slots_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  slots_gameApp.swift
+//  slots-game
+//
+//  Created by George Yin Xu on 18/9/22.
+//
+
+import SwiftUI
+
+@main
+struct slots_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
