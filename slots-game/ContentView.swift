86cbb8f1e639ae584783ad1d822e2ac3f427a3e1
George Yin Xu <georgeyinxu@gmail.com>
2022-09-18 22:19:37 +0800

    Completed slot game

--- slots-game/ContentView.swift
+++ slots-game/ContentView.swift
@@ -8,12 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+          
+    @State var firstSlot = 1
+    @State var secondSlot = 1
+    @State var thirdSlot = 1
+    @State var credits = 1000
+          
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("SwiftUI Slots!")
+                  .font(.largeTitle)
+                  .fontWeight(.bold)
+                  
+          Spacer()
+                  
+            Text("Credits: " + String(credits))
+                  
+          Spacer()
+                  
+            HStack {
+                Image("fruit\(firstSlot)")
+                      .resizable()
+                      .aspectRatio(contentMode: .fit)
+                Image("fruit\(secondSlot)")
+                      .resizable()
+                      .aspectRatio(contentMode: .fit)
+                Image("fruit\(thirdSlot)")
+                      .resizable()
+                      .aspectRatio(contentMode: .fit)
+            }
+                  
+          Spacer()
+                  
+          Button("Spin", action: {
+                    firstSlot = Int.random(in: 1...3)
+                    secondSlot = Int.random(in: 1...3)
+                    thirdSlot = Int.random(in: 1...3)
+                    
+                    if (firstSlot == secondSlot && secondSlot == thirdSlot) {
+                              credits += 25
+                    } else {
+                              credits -= 15
+                    }
+          })
+                .frame(width: 80.0, height: 30.0)
+                .foregroundColor(Color.white)
+                .background(Color.red)
+                .cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
+                .padding()
+                  
+          Spacer()
         }
         .padding()
     }
